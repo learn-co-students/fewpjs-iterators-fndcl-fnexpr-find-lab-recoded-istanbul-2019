4a1c007634769d5de7ff3a4af364ccbe6e896be8
bsurmen <busurmen@gmail.com>
2020-01-26 05:03:02 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(x){
+  const result = x.find(function(y){
+    return y["result"] === "W"
+  })
+  if (result){
+    return result.year
+  }
+}
