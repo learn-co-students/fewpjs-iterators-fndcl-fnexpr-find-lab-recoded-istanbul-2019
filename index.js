50086714246912926a23f210fed9a19d1eb23db0
lobana alarabi <loubanaeng@gmail.com>
2020-02-01 20:30:03 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"} ]
+function superbowlWin(record)
+{
+  let res=record.find(function(rec){return (rec.result == "W")});
+  if(res!==undefined)
+  {
+    console.log(res.year);
+    return res.year;
+    
+  }
+    return undefined;
+}
