0b125f1df7c4e2b87783d33eb345b20a0876cc2f
mmelihdogan <doganmmelih@gmail.com>
2020-02-04 17:19:08 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(params) {
+  let result = params.find(
+    function(arg) {
+    return arg.result === "W";
+    }
+  );
+  
+  if (result) {
+    return result.year;
+  }
+}
