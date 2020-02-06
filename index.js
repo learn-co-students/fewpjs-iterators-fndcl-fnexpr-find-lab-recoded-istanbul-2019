99b50cd0dcc3011275ca74e597ab51ceb349a5a4
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-02-06 18:08:58 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let item = arr.find((y, index) => y.result === 'W');
+  if (item === undefined) return item;
+  return item.year;
+}
+
+done();
