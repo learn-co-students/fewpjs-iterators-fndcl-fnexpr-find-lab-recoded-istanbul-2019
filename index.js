74199d239763c6accb0b9c744ce39f131a4cfd62
Recep Öztürk <turkishteacher23@gmail.com>
2020-02-09 11:16:54 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function superbowlWin(Array){
+  
+  for (let i=0;i<Array.length;i++){
+    
+    if (Array[i].result ==="W"){
+      return Array[i].year
+    }
+  }
+}
