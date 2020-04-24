9a4480cdfa6df411cd5bad40ffd365d9deef2898
aydin <aydindavut14@gmail.com>
2020-04-25 00:18:28 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" }
+  //...
+]
+function superbowlWin(input) {
+  let year = input.find(function (r) { return r.result === "W" })
+  console.log(year)
+  if (year) {
+    // console.log(year.year)
+    return year.year
+  }
+  else {
+    console.log("hi")
+    return undefined
+  }
 }
+superbowlWin(record)
