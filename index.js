67db819350dd7874b351fbb2ae4bc0079156305b
lilas-saleh <lilas.ahmad.saleh@gmail.com>
2020-04-24 16:22:09 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function superbowlWin (record) {
+let winner = record.find( ({result}) => result ==="W")
+if (winner){
+  return winner.year
+}
+else {
+  return undefined
+}
+} 
