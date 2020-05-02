5be05e1a70751cf26000f38bd6b41d5601b380aa
refica <refia.candan@gmail.com>
2020-05-02 12:20:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
-}
+// function testFunc() {
+//   return "hi"
+// }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function superbowlWin(array) {
+  let wObject = array.find( arrObject => {
+    return arrObject["result"] === "W";
+  });
+  return wObject["year"];
+};
