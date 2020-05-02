0e6efd9b6039e22eb9d250385e305c662c42f9df
Burak Selvi <burakselvi858@hotmail.com>
2020-05-02 14:23:47 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  const a = arr.find(x => x.result === "W");
+  if (a) {
+    return a.year;
+  } else {
+    return undefined;
+  }
+  // return arr.find(x => x.result === "W").year;
+}
