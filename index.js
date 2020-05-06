7959fb3248f0cba06b42dc4789d9acf0fc762ee1
Hassan <github email address>
2020-05-06 17:47:26 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const testVar = {}
+const testVar = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
 
-function testFunc() {
-  return "hi"
+]
+function superbowlWin(testArr) {
+  const isWin = testArr.find(function(s) {
+    return s.result === "W"
+  })
+  if(isWin) {
+    return isWin.year
+  }else {
+    return undefined
+  }
+
 }
