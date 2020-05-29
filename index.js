652738d96ae9ddeca832476c0561b738389d32cb
Dara <dara.dandashi@gmail.com>
2020-05-30 02:23:38 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let winningTeam= array.find(function(team){
+    return team.result === "W"
+  })
+
+  if(winningTeam){
+    return winningTeam.year
+  }
+}
+
+
+
+
+
+// function superbowlWin(array){
+//   let test= array.find(function(something){
+//     return something.result === "W";
+//   });
+//   if(test){
+//     return test.year;
+//   }
+  
+// }
