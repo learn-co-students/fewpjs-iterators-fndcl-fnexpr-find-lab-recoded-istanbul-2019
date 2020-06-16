e5c62690a3623b27e77ab1fe9683581dfdfa095b
Maher Suleyman <maher.suleyman.ms@gmail.com>
2020-06-16 16:24:36 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+function superbowlWin(record){
+  let foundObj = record.find(obj => obj.result === "W")
+  if (!!foundObj) {
+    return foundObj.year
+  }
+    
+}
